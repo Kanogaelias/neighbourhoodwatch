6313f1198e0adb784dfb490562e778c451089a0d
Kanogaelias <kanogae@gmail.com>
2019-01-09 17:54:54 +0300

    add urlpatterns for views homepage

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns=[
+       url(r'^$',views.home,name='Home'),
