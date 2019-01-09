64914239ca95218b6d64eac866ffe14c4a9219d0
Kanogaelias <kanogae@gmail.com>
2019-01-09 17:59:59 +0300

    add urlpatterns for profile and newprofile

--- app/urls.py
+++ app/urls.py
@@ -7,3 +7,5 @@ urlpatterns=[
        url(r'^$',views.home,name='Home'),
        url(r'^new_neighbourhood',views.new_neighbourhood,name='new_neighbourhood'),
        url(r'^detail/(?P<neighbourhood_id>\d+)/$' , views.neighbourhood_details, name='detail' ),
+       url(r'^profile/',views.profile, name='profile'),
+       url(r'^new/profile$', views.new_profile, name='new-profile'),
