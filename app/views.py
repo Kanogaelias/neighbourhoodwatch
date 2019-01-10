3283127cb88d00245874e360fe9194b98e52317b
Kanogaelias <kanogae@gmail.com>
2019-01-10 14:57:19 +0300

    add home request

--- app/views.py
+++ app/views.py
@@ -3,3 +3,8 @@ from django.contrib.auth.decorators import login_required
 from .models import Neighbourhood,Profile,Business,Post
 from .forms import NewNeighbourhoodForm,NewProfileForm,NewBusinessForm,NewPostForm
 from django.views import generic
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+   neighbourhood=Neighbourhood.objects.all()
+   return render(request,'index.html',{"neighbourhood":neighbourhood,})
