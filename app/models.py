a0bb3673443306cec38c0aa4a99f7db0e8ed0180
Kanogaelias <kanogae@gmail.com>
2019-01-11 18:43:59 +0300

    add s class for business with name, description, user, neighbourhood and email

--- app/models.py
+++ app/models.py
@@ -45,3 +45,26 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
 
+class Business(models.Model):
+    name=models.CharField(max_length=60)
+    description=models.CharField(max_length=200)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    neighborhood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    email=models.EmailField()
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def find_business(cls,business_id):
+        business=cls.objects.filter(id=business_id)
+        return business
+    @classmethod
+    def update_business(cls,business_id):
+        business=cls.objects.filter(id=business_id)
+        business.name=Value
+        business.save()
+        return business
