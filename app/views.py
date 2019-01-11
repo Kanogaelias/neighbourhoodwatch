bacf454982b70e5cd408c97a62428c4faeeda5be
Kanogaelias <kanogae@gmail.com>
2019-01-11 10:36:15 +0300

    add request and if statements for new_neighbourhood

--- app/views.py
+++ app/views.py
@@ -8,3 +8,17 @@ from django.views import generic
 def home(request):
    neighbourhood=Neighbourhood.objects.all()
    return render(request,'index.html',{"neighbourhood":neighbourhood,})
+@login_required(login_url='/accounts/login/')
+def new_neighbourhood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewNeighbourhoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            neighbourhood = form.save(commit=False)
+            neighbourhood.admin = current_user
+            neighbourhood.save()
+        return redirect('Home')
+
+    else:
+        form = NewNeighbourhoodForm()
+    return render(request, 'new_neighbourhood.html', {"form": form})
